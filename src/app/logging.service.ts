6a55b9df3596a766e9b6b76f1fc2c07d5e62f5bd
kiyimba1 <edwineddie54@gmail.com>
2022-07-25 21:26:34 +0300

    Ch11-routes

--- src/app/logging.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from "@angular/core";
-
-@Injectable()
-export class LoggingService {
-    logStatusChange(status: String) {
-        console.log('A server status changed, new status: ' + status);
-    }    
-}
