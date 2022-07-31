61344055c79ef8094a129ca27eca6ffcbf208707
kiyimba1 <edwineddie54@gmail.com>
2022-07-31 22:31:11 +0300

    Fake auth service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,21 +12,12 @@ import { EditServerComponent } from './servers/edit-server/edit-server.component
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
 import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AuthService } from './auth.service';
+import { AuthGuard } from './auth-gaurd.service';
 
-const appRoutes: Routes = [
-  { path: '', component: HomeComponent },
-  {
-    path: 'users', component: UsersComponent, children: [
-      { path: ':id/:name', component: UserComponent },
-  ]},
-  
-  {
-    path: 'servers', component: ServersComponent, children: [
-      { path: ':id', component: EditServerComponent },
-      { path: ':id/edit', component: EditServerComponent },
-  ] },
-  
-];
+
 
 @NgModule({
   declarations: [
@@ -36,14 +27,16 @@ const appRoutes: Routes = [
     ServersComponent,
     UserComponent,
     EditServerComponent,
-    ServerComponent
+    ServerComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(appRoutes),
+    AppRoutingModule,
+    // RouterModule.forRoot(appRoutes),
   ],
-  providers: [ServersService],
+  providers: [ServersService, AuthService, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
