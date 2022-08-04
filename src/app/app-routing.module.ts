a68f10f20483df900fc59bd0cca5eff03bb436ba
kiyimba1 <edwineddie54@gmail.com>
2022-08-04 21:21:07 +0300

    built in observable

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,54 +1,21 @@
-import { NgModule } from "@angular/core";
-import { Routes, RouterModule } from "@angular/router";
-import { AuthGuard } from "./auth-gaurd.service";
-import { ErrorPageComponent } from "./error-page/error-page.component";
-import { HomeComponent } from "./home/home.component";
-import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
-import { CanDeactivateGaurd } from "./servers/edit-server/can-deactivate-gaurd.service";
-import { EditServerComponent } from "./servers/edit-server/edit-server.component";
-import { ServerResolver } from "./servers/server/server-resolver.service";
-import { ServerComponent } from "./servers/server/server.component";
-import { ServersComponent } from "./servers/servers.component";
-import { UserComponent } from "./users/user/user.component";
-import { UsersComponent } from "./users/users.component";
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-const appRoutes: Routes = [
-  { path: '', component: HomeComponent },
-  {
-    path: 'users', component: UsersComponent, children: [
-      { path: ':id/:name', component: UserComponent },
-  ]},
-  
-  {
-    path: 'servers', canActivateChild: [AuthGuard] , component: ServersComponent, children: [
-      { path: ':id/edit', component: EditServerComponent, canDeactivate:[CanDeactivateGaurd] },
-      { path: ':id', component: ServerComponent, resolve:{server: ServerResolver} },
-      
-    ]
-  },
-//   {
-//     path: 'not-found', component: PageNotFoundComponent
-//     },
-  {
-    path: 'not-found', component: ErrorPageComponent, data: {message: 'Page not Found!'}
-  },
-//   {
-//     path: '**', redirectTo: '/not-found'
-//   }
-  {
-    path: '**', redirectTo: '/not-found'
-  }
-  
+import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'user/:id', component: UserComponent}
 ];
 
 @NgModule({
-    imports: [
-        // RouterModule.forRoot(appRoutes, {useHash: true})
-        RouterModule.forRoot(appRoutes)
-    ],
-    exports: [
-        RouterModule
-    ]
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
 })
 export class AppRoutingModule {
 
