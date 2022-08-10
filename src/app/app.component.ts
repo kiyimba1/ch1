31967af2610b937c5ccc5123c5e358e3789fea56
kiyimba1 <edwineddie54@gmail.com>
2022-08-10 22:20:49 +0300

    grouping form controls

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,8 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  defaultQuestion = 'pet';
+  answer = '';
   suggestUserName() {
     const suggestedName = 'Superuser';
   }
