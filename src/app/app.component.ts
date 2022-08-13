cb5e77b68b08daa8fec70046d6114f8e327e86d5
kiyimba1 <edwineddie54@gmail.com>
2022-08-13 09:42:20 +0300

    REactive form in typescript

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,45 +1,20 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  defaultQuestion = 'pet';
-  answer = '';
+export class AppComponent implements OnInit {
   genders = ['male', 'female'];
-  user = {
-    username: '',
-    email: '',
-    secretQuestion: '',
-    answer: '',
-    gender: ''
-  };
-  suggestUserName() {
-    const suggestedName = 'Superuser';
-    // this.signupForm.setValue({
-    //   userData:
-    // })
-    this.signupForm.form.patchValue({
-      userData: {
-        username: suggestedName
-      }
+  sugnupForm: FormGroup;
+
+  ngOnInit() {
+    this.sugnupForm = new FormGroup({
+      'username': new FormControl(null),
+      'email': new FormControl(null),
+      'gender': new FormControl('male'),
     });
-  }
-
-  @ViewChild('f') signupForm: NgForm;
-
-  // onSubmit(form: NgForm) {
-  //   console.log(form);
-    
-  // }
-
-  onSubmit() {
-    this.user.username = this.signupForm.value.userData.username;
-    this.user.username = this.signupForm.value.userData.username;
-    this.user.username = this.signupForm.value.userData.username;
-    this.user.username = this.signupForm.value.userData.username;
   }
 }
