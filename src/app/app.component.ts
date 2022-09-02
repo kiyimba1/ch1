869e930e5b5eb1ad4c7579a3a5d156e216ed197e
Edwin <edwinkiyimba@gmail.com>
2022-09-02 12:06:03 +0300

    requests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { Post } from './post.model';
+import { PostService } from './posts.service';
 
 @Component({
   selector: 'app-root',
@@ -12,48 +13,32 @@ export class AppComponent implements OnInit {
   loadedPosts: Post[] = [];
   isLoading = false;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private postsService: PostService) { }
 
   ngOnInit() {
-    this.fetchPosts();
+    this.isLoading = true;
+    this.postsService.fetchPosts().subscribe(posts => {
+      this.isLoading = false;
+      this.loadedPosts = posts;
+    });
   }
 
   onCreatePost(postData: Post) {
     // Send Http request
-    this.http.post<{ name: string }>('https://ng-complete-91e66-default-rtdb.firebaseio.com/posts.json', postData).subscribe(
-      responseData => {
-        console.log(responseData);
-      }
-    )
+    this.postsService.createAndStorePost(postData.title, postData.content);
   }
 
   onFetchPosts() {
     // Send Http request
-    this.fetchPosts();
+    this.isLoading = true;
+    this.postsService.fetchPosts().subscribe(posts => {
+      this.isLoading = false;
+      this.loadedPosts = posts;
+    });
   }
 
   onClearPosts() {
     // Send Http request
   }
 
-  private fetchPosts() {
-    this.isLoading = true;
-    this.http.get<{ [key: string]: Post }>('https://ng-complete-91e66-default-rtdb.firebaseio.com/posts.json')
-      .pipe(
-        map((response) => {
-          const postsArray: Post[] = [];
-          for (const key in response) {
-            if (response.hasOwnProperty(key)) {
-              postsArray.push({ ...response[key], id: key })
-            }
-
-          }
-          return postsArray
-        })
-      )
-      .subscribe(posts => {
-        this.isLoading = false;
-        this.loadedPosts = posts;
-      })
-  }
 }
--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -1,8 +1,34 @@
+import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { map } from "rxjs/operators";
+import { Post } from "./post.model";
 
 @Injectable({ providedIn: 'root' })
-export class PostService{
+export class PostService {
+    constructor(private http: HttpClient) { }
+
     createAndStorePost(title: string, content: string) {
-        
+        const postData: Post = { title: title, content: content }
+        this.http.post<{ name: string }>('https://ng-complete-91e66-default-rtdb.firebaseio.com/posts.json', postData).subscribe(
+            responseData => {
+                console.log(responseData);
+            }
+        )
+    }
+
+    fetchPosts() {
+        return this.http.get<{ [key: string]: Post }>('https://ng-complete-91e66-default-rtdb.firebaseio.com/posts.json')
+            .pipe(
+                map((response) => {
+                    const postsArray: Post[] = [];
+                    for (const key in response) {
+                        if (response.hasOwnProperty(key)) {
+                            postsArray.push({ ...response[key], id: key })
+                        }
+
+                    }
+                    return postsArray
+                })
+            );
     }
 }
